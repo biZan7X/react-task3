75bf9838b945b756205d3e68221dec28c72771c2
Biswanath Tewari <biswanath@geekyants.com>
2021-07-16 15:31:58 +0530

    feat - msg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const[msg,setMsg] = useState('Loading...');
+  const[theme,setTheme] = useState('green');
+
+  useEffect(()=>{
+    let time = new Date().getHours();
+
+    if(time >= 0 && time < 12){
+      setMsg('Good Morning from React. You are having coffee with me')
+      setTheme('green')
+    } else if(time >= 12 && time < 17){
+      setMsg('Good Afternoon from React. Wanna lunch with me')
+      setTheme('orange')
+    } else {
+      setMsg('Good Evening from React. Lets drink together')
+      setTheme('blue')
+    }
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{color:theme}}>{msg}</h1>
     </div>
   );
 }
